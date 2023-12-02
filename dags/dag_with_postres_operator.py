58bceaa0628b87423a067f913436334f3cbb2100
Aleksandra <aleksandra.matacz93@gmail.com>
2023-12-02 10:59:50 +0100

    dag with postgres operator added

--- dags/dag_with_postres_operator.py
+++ dags/dag_with_postres_operator.py
@@ -11,10 +11,10 @@ default_args = {
 }
 
 with DAG(
-    dag_id='dag_with_postgres_operator_v04',
+    dag_id='dag_with_postgres_operator_v06',
     default_args=default_args,
-    start_date=datetime(2023, 11, 28),
-    schedule_interval='0 0 * * *'
+    start_date=datetime(2023, 11, 28, 12, 00, 00),
+    schedule_interval='0 12 * * *'
 ) as dag:
     task1 = PostgresOperator(
         task_id='create_postgres_table',
@@ -29,10 +29,18 @@ with DAG(
 
     )
     task2 = PostgresOperator(
+        task_id='delete_data_from_table',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            delete from dag_runs where dt = '{{ ds }}' and dag_id = '{{ dag.dag_id }}'
+            """
+    )
+    task3 = PostgresOperator(
         task_id='insert_into_table',
         postgres_conn_id='postgres_localhost',
         sql="""
             insert into dag_runs (dt, dag_id) values ( '{{ ds }}', '{{ dag.dag_id }}')
         """
     )
-    task1 >> task2
+
+    task1 >> task2 >> task3
